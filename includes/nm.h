846c17faf5eb361d8c4ae19e62d0432526f082a7
tiboitel <tiboitel@e1r11p16.42.fr>
2018-04-05 22:08:32 +0200

    Update.

---
+++ includes/nm.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nm.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiboitel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/20 18:49:59 by tiboitel          #+#    #+#             */
+/*   Updated: 2017/02/20 19:24:52 by tiboitel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_NM_H
+# define FT_NM_H
+# include <stdio.j>
+# include <sys/mman.h>
+# include <mach-o/loader.h>
+# include <mach-o/nlist.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <stdlib.h>
+
+#endif
